7a11c4a77f4d1e1c792fe95a4ead9edbb61f1f71
moritalous <moritalous@gmail.com>
2021-01-03 00:48:54 +0900

    Add PlatformIO

---
+++ PlatformIO/src/main.cpp
@@ -0,0 +1,112 @@
+#include <Arduino.h>
+#include <M5Core2.h>
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char *ssid = "ssid";
+const char *password = "password";
+
+const char *root_ca =
+    "-----BEGIN CERTIFICATE-----\n"
+    "MIIEkjCCA3qgAwIBAgITBn+USionzfP6wq4rAfkI7rnExjANBgkqhkiG9w0BAQsF\n"
+    "ADCBmDELMAkGA1UEBhMCVVMxEDAOBgNVBAgTB0FyaXpvbmExEzARBgNVBAcTClNj\n"
+    "b3R0c2RhbGUxJTAjBgNVBAoTHFN0YXJmaWVsZCBUZWNobm9sb2dpZXMsIEluYy4x\n"
+    "OzA5BgNVBAMTMlN0YXJmaWVsZCBTZXJ2aWNlcyBSb290IENlcnRpZmljYXRlIEF1\n"
+    "dGhvcml0eSAtIEcyMB4XDTE1MDUyNTEyMDAwMFoXDTM3MTIzMTAxMDAwMFowOTEL\n"
+    "MAkGA1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJv\n"
+    "b3QgQ0EgMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJ4gHHKeNXj\n"
+    "ca9HgFB0fW7Y14h29Jlo91ghYPl0hAEvrAIthtOgQ3pOsqTQNroBvo3bSMgHFzZM\n"
+    "9O6II8c+6zf1tRn4SWiw3te5djgdYZ6k/oI2peVKVuRF4fn9tBb6dNqcmzU5L/qw\n"
+    "IFAGbHrQgLKm+a/sRxmPUDgH3KKHOVj4utWp+UhnMJbulHheb4mjUcAwhmahRWa6\n"
+    "VOujw5H5SNz/0egwLX0tdHA114gk957EWW67c4cX8jJGKLhD+rcdqsq08p8kDi1L\n"
+    "93FcXmn/6pUCyziKrlA4b9v7LWIbxcceVOF34GfID5yHI9Y/QCB/IIDEgEw+OyQm\n"
+    "jgSubJrIqg0CAwEAAaOCATEwggEtMA8GA1UdEwEB/wQFMAMBAf8wDgYDVR0PAQH/\n"
+    "BAQDAgGGMB0GA1UdDgQWBBSEGMyFNOy8DJSULghZnMeyEE4KCDAfBgNVHSMEGDAW\n"
+    "gBScXwDfqgHXMCs4iKK4bUqc8hGRgzB4BggrBgEFBQcBAQRsMGowLgYIKwYBBQUH\n"
+    "MAGGImh0dHA6Ly9vY3NwLnJvb3RnMi5hbWF6b250cnVzdC5jb20wOAYIKwYBBQUH\n"
+    "MAKGLGh0dHA6Ly9jcnQucm9vdGcyLmFtYXpvbnRydXN0LmNvbS9yb290ZzIuY2Vy\n"
+    "MD0GA1UdHwQ2MDQwMqAwoC6GLGh0dHA6Ly9jcmwucm9vdGcyLmFtYXpvbnRydXN0\n"
+    "LmNvbS9yb290ZzIuY3JsMBEGA1UdIAQKMAgwBgYEVR0gADANBgkqhkiG9w0BAQsF\n"
+    "AAOCAQEAYjdCXLwQtT6LLOkMm2xF4gcAevnFWAu5CIw+7bMlPLVvUOTNNWqnkzSW\n"
+    "MiGpSESrnO09tKpzbeR/FoCJbM8oAxiDR3mjEH4wW6w7sGDgd9QIpuEdfF7Au/ma\n"
+    "eyKdpwAJfqxGF4PcnCZXmTA5YpaP7dreqsXMGz7KQ2hsVxa81Q4gLv7/wmpdLqBK\n"
+    "bRRYh5TmOTFffHPLkIhqhBGWJ6bt2YFGpn6jcgAKUj6DiAdjd4lpFw85hdKrCEVN\n"
+    "0FE6/V1dN2RMfjCyVSRCnTawXZwXgWHxyvkQAiSr6w10kY17RSlQOYiypok1JR4U\n"
+    "akcjMS9cmvqtmg5iUaQqqcT5NJ0hGA==\n"
+    "-----END CERTIFICATE-----";
+
+String url = "API Gateway URL";
+
+void connectWifi()
+{
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+
+    // attempt to connect to Wifi network:
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        // wait 1 second for re-trying
+        delay(1000);
+    }
+
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+}
+
+void getPngFile()
+{
+    HTTPClient client;
+    client.setTimeout(60000);
+    client.begin(url, root_ca);
+
+    int responseCode = client.GET();
+    Serial.println(responseCode);
+
+    if (responseCode == HTTP_CODE_OK)
+    {
+        File file;
+        file = SD.open("/weather.png", FILE_WRITE);
+
+        WiFiClient *stream = client.getStreamPtr();
+
+        size_t length = 256;
+
+        uint8_t buffer[length];
+
+        while (stream->available())
+        {
+            size_t size = stream->readBytes(buffer, length);
+            file.write(buffer, size);
+        }
+        stream->stop();
+        file.close();
+    }
+    else
+    {
+        Serial.println("HTTP ERROR");
+    }
+}
+
+void setup()
+{
+    M5.begin(true, true, false, false);
+    delay(100);
+
+    Serial.begin(115200);
+    delay(100);
+
+    connectWifi();
+
+    getPngFile();
+
+    M5.Lcd.drawPngFile(SD, "/weather.png", 0, 0, 320, 240);
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    delay(100);
+}
